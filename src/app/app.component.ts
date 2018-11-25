a2ff856e0e6ccf06101548db3bf0073dc07f1c98
Kelsey M. Roche <kelsroche07@gmail.com>
2018-11-25 14:32:58 -0800

    Updated Workimg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testfrontend';
+  title = 'The Blog';
   staticPath: string = 'http://localhost:3001/blogList';
   /*staticPath: string = 'http://ec2-18-223-162-216.us-east-2.compute.amazonaws.com:3001';*/
 }
